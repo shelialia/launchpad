e39b64c640f5452e88cbe04ae4727863afbed38f
shelialia <teo.zihui.shelia@gmail.com>
2025-03-09 22:16:45 +0800

    Update custom errors to inherit from HTTPException

--- app/models/responses.py
+++ app/models/responses.py
@@ -1,18 +1,13 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from fastapi import HTTPException, status
 
 
-class APIError(BaseModel):
+class APIError(HTTPException):
     """Generic schema for API Errors."""
 
-    code: int = Field(..., description="API Error code associated with the error")
-    message: str = Field(..., description="Error message associated with the error")
-    request: Optional[dict] = Field(
-        None, description="Request details associated with the error"
-    )
-    details: Optional[dict] = Field(
-        None, description="Other details associated with the error"
-    )
+    def __init__(code: int, detail: str):
+        super().__init__(status_code=code, detail=detail)
 
 
 class CreatedResponse(BaseModel):
@@ -40,36 +35,26 @@ class DeletedResponse(BaseModel):
 class InvalidParametersError(APIError):
     """Error response for invalid parameters (400 Bad Request)."""
 
-    code: int = Field(400, description="HTTP Status Code for Invalid Parameters")
-    message: str = Field(
-        "Invalid parameters provided", description="Error message for invalid input"
-    )
+    def __init__(detail: str):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
 
 
 class NotFoundError(APIError):
     """Error response when a resource is not found (404 Not Found)."""
 
-    code: int = Field(404, description="HTTP Status Code for Not Found")
-    message: str = Field(
-        "Specified resource(s) was not found",
-        description="Error message when resource is missing",
-    )
+    def __init__(detail: str):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
 
 
 class InvalidCreationError(APIError):
     """Error response when a resource cannot be created (422 Unprocessable Entity)."""
 
-    code: int = Field(422, description="HTTP Status Code for Invalid Creation")
-    message: str = Field(
-        "Unable to create resource",
-        description="Error message when resource creation fails",
-    )
+    def __init__(detail: str):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
 
 
 class InternalServerError(APIError):
     """Error response for server issues (500 Internal Server Error)."""
 
-    code: int = Field(500, description="HTTP Status Code for Internal Server Error")
-    message: str = Field(
-        "Internal server error", description="Error message for internal issues"
-    )
+    def __init__():
+        super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
