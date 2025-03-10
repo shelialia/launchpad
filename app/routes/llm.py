1bf989cd5560833dafc6707bf630245230d3e0fe
shelialia <teo.zihui.shelia@gmail.com>
2025-03-10 10:38:26 +0800

    Update parameter model to remove unnecessary models

--- app/routes/llm.py
+++ app/routes/llm.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.services.llm_service import send_prompt
 from app.models.conversation import Prompt
 from app.models.exceptions import InternalServerError
+from app.models.parameters import IDParam
 
 router = APIRouter()
 
 
 @router.post("/{conversation_id}")
-async def chat_with_llm(conversation_id: int, prompt: Prompt):
+async def chat_with_llm(conversation_id: IDParam, prompt: Prompt):
     try:
         response = await send_prompt(conversation_id, prompt.content)
         return {"response": response}
